4a02a5828099a7b09576cbab823d5e5ea726a757
Bruno Domingues <brunood07@gmail.com>
2022-02-09 13:38:30 -0300

    tela de signin feita

--- App.tsx
+++ App.tsx
@@ -14,8 +14,11 @@ import {
   Poppins_700Bold 
 } from "@expo-google-fonts/poppins";
 
+import { AuthProvider } from "./src/hooks/auth";
 import theme from './src/global/styles/theme';
 import { AppRoutes } from "./src/routes/app.routes";
+
+import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -33,7 +36,9 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <NavigationContainer>
           <StatusBar barStyle="light-content" />
-          <AppRoutes />
+          <AuthProvider>
+            <SignIn />
+          </AuthProvider>
         </NavigationContainer>
       </ThemeProvider>
     </GestureHandlerRootView>
